be130789f3a03d7bd9af2827da3710d00f385938
diogocsouza <diogocsouza@users.noreply.github.com>
2020-11-19 18:43:34 -0300

    .

--- ProjetoWeb/index.js
+++ ProjetoWeb/index.js
@@ -3,16 +3,25 @@ const express = require('express')
 const app = express()
 
 //null
-const handlebars = require('handlebars')
+var handlebars = require('express-handlebars')
+
+
+//null 
+const bodyParser = require('body-parser')
 
 //biblioteca para realizar conexão com o bd
 const Sequelize = require("sequelize")
+
 
 
 //Config
     //Templete engine (handlebars)
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+
+    //BodyParser
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
 
     //conexão com o bd
     const sequelize = new Sequelize('test', 'root', 'admin', {
@@ -23,7 +32,14 @@ const Sequelize = require("sequelize")
 
 //Rotas
     app.get('/login', function(req, res){
-        res.send("Loga ai bicho")
+        res.render('login')
+    })
+
+    app.post('/efetuarlogin', function(req, res){
+        let login = req.body.login
+        res.send('Login efetuado com sucesso '+login)
+
+
     })
 
 //deve sempre ser a ultima linha do codigo, inicia servidor
